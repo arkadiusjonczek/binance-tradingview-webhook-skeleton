4611ed00531d9ec9814eff9b43feaf2b0312d5ec
Arkadius Jonczek <7071411+arkadiusjonczek@users.noreply.github.com>
2023-02-16 00:10:21 +0100

    add webhook

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # php-tradingview-binance-webhook-skeleton
+
+A minimum viable webhook for tradingview alerts to trade on binance written in php.
+
+## Requirements
+
+1. Webhosting for php webhook (e.g. uberspace.de if you are located in germany)
+2. Binace API and secret key (get webhosting first because you need to add IP address of webserver to unlock trading for the API key)
+3. TradingView alerts (you can create a strategy, backtest it and create an alert which calls the php webhook :))
+
+## TradingView Alert
+
+Add the following message to your alert and set webhook url in the notifcations.
+
+```
+{{ticker}},{{strategy.order.action}},{{strategy.order.price}}
+```
+
+The {{strategy.order.price}} is optional but you can use it later to compare price of strategy with price of fulfilled order.
---
+++ webhook.php
@@ -0,0 +1,52 @@
+<?php
+
+$allowedIps = [
+  '52.89.214.238',
+  '34.212.75.30',
+  '54.218.53.128',
+  '52.32.178.7'
+];
+
+// only allow requests from tradingview servers
+if (!in_array($_SERVER['REMOTE_ADDR'], $allowedIps)) {
+  die('not allowed');
+}
+
+$input = file_get_contents('php://input');
+
+$inputArray = explode(',', $input);
+$symbol = $inputArray[0];
+$side = $inputArray[1];
+$quantity = 0.001; // increase if you want but first test your strategy (risk management)
+
+$data = [
+  'symbol'     => $symbol,
+  'side'       => $side,
+  'type'       => 'MARKET',
+  'quantity'   => $quantity,
+  'recvWindow' => '5000',
+  'timestamp'  => floor(microtime(true) * 1000)
+];
+
+$totalParams = \http_build_query($data);
+
+// add your binance api and secret key with permissions to trade
+$apiKey = '';
+$secretKey = '';
+
+$signature = hash_hmac('sha256', $totalParams, $secretKey);
+
+$data['signature'] = $signature;
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://api.binance.com/api/v3/order');
+curl_setopt($ch, CURLOPT_HTTPHEADER, [sprintf('X-MBX-APIKEY: %s', $apiKey)]);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, \http_build_query($data));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+$httpResponseCode = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
+
+// do some more stuff if you want
